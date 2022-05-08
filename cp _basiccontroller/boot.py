30518ddf9de477550cc79516aceb5490c53cdc9e
Marci <marcacoyles@sky.com>
2022-05-08 19:35:45 +0100

    Delete cp _basiccontroller directory

--- cp _basiccontroller/boot.py
+++
@@ -1,7 +0,0 @@
-# this goes in boot.py not code.py!
-new_name = "PADMATRIX1"
-import storage
-storage.remount("/", readonly=False)
-m = storage.getmount("/")
-m.label = new_name
-storage.remount("/", readonly=True)
